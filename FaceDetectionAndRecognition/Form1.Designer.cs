e720556200e43fdd12d457095e99ae834ea15bac
Mustafa Alparslan pamuk <appo1995@hotmail.com>
2019-04-04 14:01:45 +0300

    Face Recognition system

---
+++ FaceDetectionAndRecognition/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace FaceDetectionAndRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cameraBox = new Emgu.CV.UI.ImageBox();
+            this.start = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cameraBox
+            // 
+            this.cameraBox.Location = new System.Drawing.Point(28, 0);
+            this.cameraBox.Name = "cameraBox";
+            this.cameraBox.Size = new System.Drawing.Size(318, 438);
+            this.cameraBox.TabIndex = 2;
+            this.cameraBox.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(419, 76);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(130, 36);
+            this.start.TabIndex = 3;
+            this.start.Text = "Start Detection And Recognition";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(419, 230);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(130, 59);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save Face";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(419, 204);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(130, 20);
+            this.textName.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(363, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.cameraBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox cameraBox;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FaceDetectionAndRecognition/Form1.cs
@@ -0,0 +1,128 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceDetectionAndRecognition
+{
+    public partial class Form1 : Form
+    {
+        //Declare Variables
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+        HaarCascade faceDetected;
+        Image<Bgr, Byte> Frame;
+        Capture camera;
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> Users = new List<string>();
+        int Count, NumLables, t;
+        string name, names = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //haarcascade is for face detection
+            faceDetected = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                string Labelsinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+                string[] Labels = Labelsinf.Split(',');
+                //the first label before ',' will be the number of faces saved
+                NumLables = Convert.ToInt16(Labels[0]);
+                Count = NumLables;
+                string FacesLoad;
+                for (int i = 1; i < NumLables + 1; i++)
+                {
+                    FacesLoad = "face" + i + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/Faces.txt"));
+                    labels.Add(Labels[i]);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Nothing is in the Database");
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Count = Count + 1;
+            grayFace = camera.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            MCvAvgComp[][] DetectedFaces = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach (MCvAvgComp f in DetectedFaces[0])
+            {
+                TrainedFace = Frame.Copy(f.rect).Convert<Gray, byte>();
+                break;
+            }
+            TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            trainingImages.Add(TrainedFace);
+            labels.Add(textName.Text);
+            File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+            for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+            {
+                trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/face" + i + ".bmp");
+                File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", labels.ToArray()[i-1] + ",");
+
+            }
+            MessageBox.Show(textName.Text + "Added Succesfully");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            camera = new Capture();
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcedure);
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            Users.Add("");
+            Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayFace = Frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectedNow = grayFace.DetectHaarCascade(faceDetected,1.2,10,Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20,20));
+            foreach (MCvAvgComp f in facesDetectedNow[0])
+            {
+                result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100,100,Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if(trainingImages.ToArray().Length != 0)
+                {
+                    MCvTermCriteria termCriterias = new MCvTermCriteria(Count, 0.001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 1500, ref termCriterias);
+                    name = recognizer.Recognize(result);
+                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+                }
+                
+                Users.Add("");
+            }
+            cameraBox.Image = Frame;
+            names = "";
+            Users.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
